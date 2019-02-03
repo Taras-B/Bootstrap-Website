0c5fdfb6009603a783645099e53b1196768ff673
Taras-B <taras5475@gmail.com>
2019-02-03 15:03:39 +0200

    Add animation

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
+
+    AOS.init({
+        duration: 1000,
+        delay: 0,
+    });
+
     $(document).scroll(function(){
         $('.navbar').toggleClass('scrolled', $(this).scrollTop() > $('.navbar').height());
     });
+
+
 });
